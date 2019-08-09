f260522488a236b0acb53eb78aadb838208dd0b8
Carlos Azaustre <cazaustre@gmail.com>
2019-08-09 21:40:32 +0200

    linter and deploy on now

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   output: {
-    filename: "app.bundle.js"
+    filename: 'app.bundle.js'
   },
   plugins: [new HtmlWebpackPlugin({
     template: 'src/index.html'
@@ -19,4 +19,4 @@ module.exports = {
       }
     ]
   }
-};
+}
