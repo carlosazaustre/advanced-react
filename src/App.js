9e250baecaf00962d9f3c835b551e03d954bc562
Carlos Azaustre <cazaustre@gmail.com>
2019-08-10 00:32:45 +0200

    Category and ListOfCategories components

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { ListOfCategories } from './components/ListOfCategories'
+import { GlobalStyles } from './GlobalStyles'
+
+export const App = () => (
+  <>
+    <GlobalStyles />
+    <ListOfCategories />
+  </>
+)
