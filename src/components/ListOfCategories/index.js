1959233edcdcc6109d3137e62e74e1cde2d7f8a7
Carlos Azaustre <cazaustre@gmail.com>
2019-08-10 01:21:11 +0200

    hooks

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Category } from '../Category'
 import { List, Item } from './styles'
 
-import { categories } from '../../../api/db.json'
-
 export const ListOfCategories = () => {
+  const [categories, setCategories] = useState([])
+
+  useEffect(function () {
+    window.fetch('https://petgram-server-cazaustre-41l1j4pp2.now.sh/categories')
+      .then(res => res.json())
+      .then(response => {
+        setCategories(response)
+      })
+  }, [])
+
   return (
     <List>
       {
