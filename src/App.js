ae8b7c9baaf518da10e7134036e1164ef83152ae
Carlos Azaustre <cazaustre@gmail.com>
2019-08-10 01:07:15 +0200

    animations w/ keyframes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { ListOfCategories } from './components/ListOfCategories'
 import { ListOfPhotoCards } from './components/ListOfPhotoCards'
 import { Logo } from './components/Logo'
-import { GlobalStyles } from './GlobalStyles'
+import { GlobalStyles } from './styles/GlobalStyles'
 
 export const App = () => (
   <>
