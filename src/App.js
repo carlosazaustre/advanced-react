10c58541d4507c80e4fd531a830397a8534b5ea1
Carlos Azaustre <cazaustre@gmail.com>
2019-08-10 00:57:11 +0200

    Logo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import { ListOfCategories } from './components/ListOfCategories'
+import { ListOfPhotoCards } from './components/ListOfPhotoCards'
+import { Logo } from './components/Logo'
 import { GlobalStyles } from './GlobalStyles'
 
 export const App = () => (
   <>
     <GlobalStyles />
+    <Logo />
     <ListOfCategories />
+    <ListOfPhotoCards />
   </>
 )
---
+++ src/components/PhotoCard/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { MdFavoriteBorder } from 'react-icons/md'
+import { ImgWrapper, Img, Button } from './styles'
+
+const DEFAULT_IMAGE = 'https://images.unsplash.com/photo-1500879747858-bb1845b61beb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+
+export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
+  return (
+    <article>
+      <a href={`/detail/${id}`}>
+        <ImgWrapper>
+          <Img src={src} />
+        </ImgWrapper>
+      </a>
+      <Button>
+        <MdFavoriteBorder size='32px' /> {likes} likes!
+      </Button>
+    </article>
+  )
+}
