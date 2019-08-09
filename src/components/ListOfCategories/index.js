63f2d25e4d7aa194a6d0dab657b41d56ebacb588
Carlos Azaustre <cazaustre@gmail.com>
2019-08-10 01:41:26 +0200

    customHooks

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -2,17 +2,26 @@ import React, { useState, useEffect, Fragment } from 'react'
 import { Category } from '../Category'
 import { List, Item } from './styles'
 
-export const ListOfCategories = () => {
+function useCategoriesData () {
   const [categories, setCategories] = useState([])
-  const [showFixed, setShowFixed] = useState(false)
+  const [loading, setLoading] = useState(false)
 
   useEffect(function () {
+    setLoading(true)
     window.fetch('https://petgram-server-cazaustre-41l1j4pp2.now.sh/categories')
       .then(res => res.json())
       .then(response => {
         setCategories(response)
+        setLoading(false)
       })
   }, [])
+
+  return { categories, loading }
+}
+
+export const ListOfCategories = () => {
+  const { categories, loading } = useCategoriesData()
+  const [showFixed, setShowFixed] = useState(false)
 
   useEffect(function () {
     const onScroll = e => {
@@ -26,9 +35,11 @@ export const ListOfCategories = () => {
   }, [showFixed])
 
   const renderList = (fixed) => (
-    <List className={fixed ? 'fixed' : ''}>
+    <List fixed={fixed}>
       {
-        categories.map(category => <Item key={category}><Category {...category} /></Item>)
+        loading
+          ? <Item key='loading'><Category /></Item>
+          : categories.map(category => <Item key={category}><Category {...category} /></Item>)
       }
     </List>
   )
--- src/components/ListOfCategories/styles.js
+++ src/components/ListOfCategories/styles.js
@@ -1,23 +1,25 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
 export const List = styled.ul`
   display: flex;
   overflow: scroll;
   width: 100%;
-  &.fixed {
-    background: #fff;
-    border-radius: 60px;
-    box-shadow: 0 0 20px rgba(0, 0, 0, .3);
-    left: 0;
-    margin: 0 auto;
-    max-width: 400px;
-    padding: 5px;
-    position: fixed;
-    right: 0;
-    top: -20px;
-    transform:  scale(.5);
-    z-index: 1;
-  }
+  ${props => props.fixed && css`
+    {
+      background: #fff;
+      border-radius: 60px;
+      box-shadow: 0 0 20px rgba(0, 0, 0, .3);
+      left: 0;
+      margin: 0 auto;
+      max-width: 400px;
+      padding: 5px;
+      position: fixed;
+      right: 0;
+      top: -20px;
+      transform:  scale(.5);
+      z-index: 1;
+    }
+  `}
 `
 
 export const Item = styled.li`
